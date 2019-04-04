e35954932a57a250d6700ab49fb967fcd8535834
Pedro Maia <pedro.maia@ccc.ufcg.edu.br>
2019-04-04 10:06:31 -0300

    added text

--- ri_lab_01/spiders/oantagonista.py
+++ ri_lab_01/spiders/oantagonista.py
@@ -18,28 +18,16 @@ class OantagonistaSpider(scrapy.Spider):
         self.start_urls = list(data.values())
 
     def parse(self, response):
-        #
-        # inclua seu código aqui
-        #
-        
-        
+ 
         for article in response.css('article'):
-            x = yield {
-				'titulo': article.css('div.container-post-home a.article_link::attr("title")').get(),
-                'secao': article.css('div.container-post-home a.article_link span.postmeta span.categoria').get(),
-                'data': article.css('div.container-post-home a.article_link span.postmeta time').get(),
-                'autor': article.css('div.container-post-home a.article_link span.postmeta div').get(),
-                'texto': article.css('div.container-post-home a.article_link p').get(),
+            yield {
+				'titulo': article.css('div.container-post-home a.article_link h2::text').get(),
+                'secao': article.css('div.container-post-home a.article_link span.postmeta span.categoria::text').get(),
+                'data': article.css('div.container-post-home a.article_link span.postmeta time::text').get(),
+                'autor': article.css('div.container-post-home a.article_link span.postmeta div::text').get(),
+                'texto': article.css('div.container-post-home a.article_link p::text').get(),
                 'url': article.css('div.container-post-home a.article_link::attr("href")').get()
             }
-            print '\n'
-            print 'Título :   ' + x['titulo']
-            print 'Seção :   ' + x['secao']
-            print 'Data :   ' + x['data']
-            print 'Autor :   ' + x['autor']
-            print 'Texto :   ' + x['texto']
-            print 'URL :   ' + x['url']
-            print '\n'
         page = response.url.split("/")[-2]
         filename = 'quotes-%s.html' % page
         with open(filename, 'wb') as f:

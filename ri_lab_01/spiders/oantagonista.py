0889fe2eed96f23e205f121c1cafea025b58aa0d
pedrohcm <pedro.maia@ccc.ufcg.edu.br>
2019-04-05 11:45:36 -0300

    fix date

--- ri_lab_01/spiders/oantagonista.py
+++ ri_lab_01/spiders/oantagonista.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import json
+import datetime
 
 from ri_lab_01.items import RiLab01Item
 from ri_lab_01.items import RiLab01CommentItem
@@ -10,6 +11,9 @@ class OantagonistaSpider(scrapy.Spider):
     name = 'oantagonista'
     allowed_domains = ['oantagonista.com']
     start_urls = []
+    pageStart = 1
+    limitDate = datetime.datetime(2018,1,1)
+
 
     def __init__(self, *a, **kw):
         super(OantagonistaSpider, self).__init__(*a, **kw)
@@ -17,23 +21,76 @@ class OantagonistaSpider(scrapy.Spider):
                 data = json.load(json_file)
         self.start_urls = list(data.values())
 
+    
     def parse(self, response):
- 
-        for article in response.css('article'):
-            yield {
-				'titulo': article.css('div.container-post-home a.article_link h2::text').get(),
-                'secao': article.css('div.container-post-home a.article_link span.postmeta span.categoria::text').get(),
-                'data': article.css('div.container-post-home a.article_link span.postmeta time::text').get(),
-                'autor': article.css('div.container-post-home a.article_link span.postmeta div::text').get(),
-                'texto': article.css('div.container-post-home a.article_link p::text').get(),
-                'url': article.css('div.container-post-home a.article_link::attr("href")').get()
-            }
+
+        def checkDateLimit(response, lowerLimit):
+            currentDate = response.css('time.entry-date ::attr(datetime)').get()
+            currentDate = currentDate.split(' ')[0].split('-')
+
+            date =  datetime.datetime(int(currentDate[-3]), int(currentDate[-2]), int(currentDate[-1]))
+
+            return date > lowerLimit
+
+
+        if not checkDateLimit(response, self.limitDate) :
+            return
+
+        articles = response.css('article')
+
+        for href in articles.css('a.article_link::attr(href)'):
+            yield response.follow(href, callback= self.scrapeNewsPage)
+
         page = response.url.split("/")[-2]
+
+        nextPage = self.start_urls[0] + 'pagina/' + str(self.pageStart) +'/?ajax'
+        self.pageStart = self.pageStart + 1
+
+
         filename = 'quotes-%s.html' % page
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
+
         
-        #
-        #
-        #
+        yield scrapy.Request(nextPage, callback=self.parse)
+
+
+    def scrapeNewsPage(self, response):
+
+        def getArticleText(response):
+
+            articleText = ""
+
+            for paragraph in response.css('div.entry-content p::text'):
+                articleText = articleText + paragraph.get()
+
+            return articleText
+            
+        def reverseDate(date):
+            date,time = date.split(" ")
+            adjustedDate = date.split("-")
+            adjustedDate.reverse()
+            formattedDate = '/'.join(adjustedDate)	
+            return formattedDate + " " + time
+
+        def encodeUTF8(target):
+            return target.encode('UTF-8')
+
+        def getArticleAuthor(field):
+            articleAuthor = field.replace('Por ','')
+            return encodeUTF8(articleAuthor) if articleAuthor.strip() else "Sem autor"
+
+        yield {
+            'title': encodeUTF8(response.css('h1.entry-title::text').get()),
+            # This source does not provide subtitles
+            'subtitle': 'None',
+            # Some articles have no author
+            'author': getArticleAuthor(response.css('header.entry-header div::text').get()),
+            # Formats date to match (dd/mm/yyyy hh:mi:ss)
+            'date': encodeUTF8(reverseDate(response.css('time.entry-date ::attr(datetime)').get())),
+            'section': encodeUTF8(response.css('header.entry-header span.postmeta span.categoria a::text').get()),
+            # Joins paragraphs
+            'text': encodeUTF8(getArticleText(response)),
+            'url': response.request.url
+        }
